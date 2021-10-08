9555ea70e000242d00d353d15cd6ebe595ccd095
dsaadatmandi <40693407+dsaadatmandi@users.noreply.github.com>
2021-10-08 17:11:25 +0300

    Delete keyledger.py
    keys shown

--- utils/keyledger.py
+++
@@ -1,9 +0,0 @@
-# For Googlemaps Places query functionality and certain tile providers, you need an API key.
-# All of these are available online for free, up to a certain quota (generally 1000s of monthly requests free).
-
-
-class KeyLedger():
-    def __init__(self):
-        self.googlemaps_key = "AIzaSyCBwABBvKMoD-tTTZmuw_Io-EgBQemN7wQ"
-        self.thunderforest_key = "4811d9ca2dcc4ac5a597a70cb6e263cb"
-        self.stadia_key = "6791cad9-2687-433b-a86e-72ef2166907d"
